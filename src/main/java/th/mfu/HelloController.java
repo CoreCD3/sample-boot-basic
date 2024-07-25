62d9176810ce1b1cf5aec930c2ae3a4cf982bb59
CoreCD3 <talae1111@hotmail.com>
2024-07-25 15:00:51 +0700

    uwu

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,7 +1,5 @@
 package th.mfu;
 
-import javax.print.DocFlavor.STRING;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,24 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/secure")
 public class HelloController {
 
     @GetMapping("/greet")
     String hello() {
-        return "Hello Java!";
+        return "Hello World!";
     }
 
-    String[] valiNames = {"nacha","john","maria"};
+
+    String[] validNames = {"nacha", "john","maria"};
 
     @GetMapping("/hi/{name}")
     ResponseEntity<String> hi(@PathVariable String name){
-        //return new ResponseEntity<> ("Hi "+name , HttpStatus.OK)
-		//return ResponseEntity.ok("Hi "+name );
-        for(String validName : valiNames){
-            if(validName.equals(name)){
-                return ResponseEntity.ok("Hi "+name );
-            }
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
- 	}
+       // return new ResponseEntity<>("Hi "+name, HttpStatus.OK);
+      //  return ResponseEntity.ok("Hi "+ name);
+      for(String validName : validNames){
+         if(validName.equals(name))
+            return ResponseEntity.ok("Hi "+ name);
+      }
+     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+   
 }
